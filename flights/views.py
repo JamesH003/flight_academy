70c9b1bcef2431f3da6d91b0160154772dac2c37
James Horgan <jamesrhorgan@gmail.com>
2024-01-23 21:01:34 +0000

    create vouchers view and template

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Voucher
 
 # Create your views here.
+
+def all_vouchers(request):
+    """ A view to show all vouchers """
+
+    vouchers = Voucher.objects.all()
+
+    context = {
+        'vouchers': vouchers,
+    }
+    
+    return render(request, 'flights/vouchers.html', context)
