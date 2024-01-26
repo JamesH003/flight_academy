1b52b040ed632ac6034b3203c75fbd11de734509
James Horgan <jamesrhorgan@gmail.com>
2024-01-26 19:20:23 +0000

    create add_voucher view

--- flights/urls.py
+++ flights/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_vouchers, name='vouchers'),
-    path('<voucher_id>', views.voucher_detail, name='voucher_detail'),
+    path('add_voucher/', views.add_voucher, name='add_voucher'),
+    path('<voucher_id>/', views.voucher_detail, name='voucher_detail'),
 ]
