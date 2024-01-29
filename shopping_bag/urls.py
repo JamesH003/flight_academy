b1abbcfbe67e0e192cf361d01dd0dbbfdf7897f9
James Horgan <jamesrhorgan@gmail.com>
2024-01-29 17:01:21 +0000

    create add_to_shopping_bag functionality

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_shopping_bag, name='view_shopping_bag'),
+    path('add/<voucher_id>', views.add_to_shopping_bag, name='add_to_shopping_bag'),
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ def view_shopping_bag(request):
     """ A view that renders the shopping bag contents page """
     
     return render(request, 'shopping_bag/shopping_bag.html')
+
+
+def add_to_shopping_bag(request, voucher_id):
+    """ Add a quantity of the specified voucher to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if voucher_id in list(shopping_bag.keys()):
+        shopping_bag[voucher_id] += quantity
+    else:
+        shopping_bag[voucher_id] = quantity
+
+    request.session['shopping_bag'] = shopping_bag
+    print(request.session['shopping_bag'])
+    return redirect(redirect_url)
