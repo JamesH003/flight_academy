10542e0040640c8ab5fcdebfd9b3d82533f5a19b
James Horgan <jamesrhorgan@gmail.com>
2024-01-31 21:08:57 +0000

    create url to adjust voucher quantity

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.view_shopping_bag, name='view_shopping_bag'),
     path('add/<voucher_id>', views.add_to_shopping_bag, name='add_to_shopping_bag'),
+    path('adjust/<voucher_id>', views.adjust_shopping_bag, name='adjust_shopping_bag'),
 ]
