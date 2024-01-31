8a2c857556b6760e1d64fab2cf8d78433b76ce43
James Horgan <jamesrhorgan@gmail.com>
2024-01-31 20:45:31 +0000

    create view for the payments app

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def payments(request):
+    shopping_bag = request.session.get('shopping_bag', {})
+    if not shopping_bag:
+        messages.error(request, "Your shopping bag is currently empty")
+        return redirect(reverse('vouchers'))
+
+    order_form = OrderForm()
+    template = 'payments/payments.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
