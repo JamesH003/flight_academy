c71b3b1d7827243560dabb96e80edfcc18dd91cb
James Horgan <jamesrhorgan@gmail.com>
2024-01-31 21:09:14 +0000

    create url to remove voucher

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.view_shopping_bag, name='view_shopping_bag'),
     path('add/<voucher_id>', views.add_to_shopping_bag, name='add_to_shopping_bag'),
     path('adjust/<voucher_id>', views.adjust_shopping_bag, name='adjust_shopping_bag'),
+    path('remove/<voucher_id>', views.remove_from_shopping_bag, name='remove_from_shopping_bag'),
 ]
