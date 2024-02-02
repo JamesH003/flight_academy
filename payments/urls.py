6bf07813fa731754cb5c95ca09f8a72d77e7236d
James Horgan <jamesrhorgan@gmail.com>
2024-02-02 14:40:52 +0000

    create url for payments_success

--- payments/urls.py
+++ payments/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.payments, name='payments'),
+    path('payments_success/<order_number>', views.payments_success, name='payments_success'),
 ]
