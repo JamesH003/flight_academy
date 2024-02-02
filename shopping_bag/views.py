dfba084e85aa6ea3b076505078c9c2026a339de4
James Horgan <jamesrhorgan@gmail.com>
2024-02-02 14:43:43 +0000

    add success and error messages to views.py

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -4,23 +4,28 @@ from django.contrib import messages
 from flights.models import Voucher
 
 
+# view to view the shopping bag
 def view_shopping_bag(request):
     """ A view that renders the shopping bag contents page """
     
     return render(request, 'shopping_bag/shopping_bag.html')
 
 
+# view to add vouchers to the shopping bag
 def add_to_shopping_bag(request, voucher_id):
     """ Add a quantity of the specified voucher to the shopping bag """
 
+    voucher = get_object_or_404(Voucher, pk=voucher_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     shopping_bag = request.session.get('shopping_bag', {})
 
     if voucher_id in list(shopping_bag.keys()):
         shopping_bag[voucher_id] += quantity
+        messages.success(request, f'Updated {voucher.title} quantity to {voucher.title} quantity to {shopping_bag[voucher_id]}')
     else:
         shopping_bag[voucher_id] = quantity
+        messages.success(request, f'Added {voucher.title} to your shopping bag')
 
     request.session['shopping_bag'] = shopping_bag
     return redirect(redirect_url)
@@ -53,9 +58,11 @@ def remove_from_shopping_bag(request, voucher_id):
         voucher = get_object_or_404(Voucher, pk=voucher_id)
         shopping_bag = request.session.get('shopping_bag', {})
         shopping_bag.pop(voucher_id)
+        messages.success(request, f'Removed {voucher.title} from your shopping bag')
 
         request.session['shopping_bag'] = shopping_bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
