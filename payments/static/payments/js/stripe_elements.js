dccb2641aff228533fcbd3247cf461aaafa3d396
James Horgan <jamesrhorgan@gmail.com>
2024-02-02 14:38:35 +0000

    fix typos in stripe_elements.js

--- payments/static/payments/js/stripe_elements.js
+++ payments/static/payments/js/stripe_elements.js
@@ -1,14 +1,14 @@
-/* 
+/*
     Core logic/payment flow for this comes from here:
-    https://stripe.come/docs/payments/accept-a-payment
+    https://stripe.com/docs/payments/accept-a-payment
 
-    CSS from here:
+    CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
-var stripPublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripPublicKey);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -25,9 +25,7 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {
-    style: style
-});
+var card = elements.create('card', { style: style });
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
@@ -42,7 +40,7 @@ card.addEventListener('change', function (event) {
         `;
         $(errorDiv).html(html);
     } else {
-        errorDiv.textContent = ''
+        errorDiv.textContent = '';
     }
 });
 
@@ -51,28 +49,22 @@ var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function (ev) {
     ev.preventDefault();
-    card.update({
-        'disabled':
-        true
-    });
-    $('#submit-button').attr('disabled', true)
+    card.update({ 'disabled': true });
+    $('#submit-button').attr('disabled', true);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
-            card: card
+            card: card,
         }
     }).then(function (result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
             var html = `
                 <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
-            card.update({
-                'disabled':
-                false
-            });
+            card.update({ 'disabled': false });
             $('#submit-button').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
