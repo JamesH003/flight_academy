b08ca12a39f36efe76b3903f5cc15c58b7e1fb33
James Horgan <jamesrhorgan@gmail.com>
2024-02-02 15:16:09 +0000

    create loading page within payment flow

--- payments/static/payments/js/stripe_elements.js
+++ payments/static/payments/js/stripe_elements.js
@@ -51,6 +51,8 @@ form.addEventListener('submit', function (ev) {
     ev.preventDefault();
     card.update({ 'disabled': true });
     $('#submit-button').attr('disabled', true);
+    $('#payments-form').fadeToggle(100);
+    $('#loading-page-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -64,6 +66,8 @@ form.addEventListener('submit', function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payments-form').fadeToggle(100);
+            $('#loading-page-overlay').fadeToggle(100);
             card.update({ 'disabled': false });
             $('#submit-button').attr('disabled', false);
         } else {
