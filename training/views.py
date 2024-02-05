cf5119b39d13773eb0c5edb0625c8f873b095400
James Horgan <jamesrhorgan@gmail.com>
2024-02-05 23:13:19 +0000

    create view for training.html

--- training/views.py
+++ training/views.py
@@ -1,1 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Training
+
+from flights.models import Aircraft
+# from .forms import TrainingForm
+
+
+def training(request):
+    """ A view to show all training courses """
+
+    training = Training.objects.all()
+
+    context = {
+        'training': training,
+    }
+    
+    return render(request, 'training/training.html', context)
