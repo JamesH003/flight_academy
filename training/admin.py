a03bb6bf877b35d9e03df2022fff13fcf6ed21c3
James Horgan <jamesrhorgan@gmail.com>
2024-02-05 23:13:47 +0000

    register Training and Licence in admin.py

--- training/admin.py
+++ training/admin.py
@@ -1,1 +1,32 @@
 from django.contrib import admin
+from .models import Training, Licence
+
+
+class TrainingAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'licence_type',
+        'cost',
+        'mode_of_training',
+        'aircraft_type',
+        'image',
+    )
+
+    ordering = ('cost',)
+
+
+class LicenceAdmin(admin.ModelAdmin):
+    list_display = (
+        'licence_type',
+        'required_flight_time',
+        'engines',
+    )
+
+    ordering = ('required_flight_time',)
+
+
+admin.site.register(Training, TrainingAdmin)
+admin.site.register(Licence, LicenceAdmin)
+
+
