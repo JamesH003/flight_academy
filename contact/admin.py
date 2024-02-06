77bd54758dbf644a0c7637901d498525b1d4a39a
James Horgan <jamesrhorgan@gmail.com>
2024-02-06 12:09:56 +0000

    create ContactAdmin in admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.db import models
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("msg_date", "first_name", "last_name", "email", "phone_num", "specific_course", "message",)
+    list_filter = ("email", )
+    search_fields = ["email", "last_name"]
