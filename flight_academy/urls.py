0a1421e5061acd2ba25928450e75cc1d919a7e6a
James Horgan <jamesrhorgan@gmail.com>
2024-02-10 00:37:38 +0000

    Create custom error handling with 404 and 500 pages

--- flight_academy/urls.py
+++ flight_academy/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +33,6 @@ urlpatterns = [
     path('newsletter/', include('newsletter.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'flight_academy.views.handler404'
+handler500 = 'flight_academy.views.handler500'
